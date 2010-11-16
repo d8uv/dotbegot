dbdd863b7dbac8ca5ad3fd8f35270cf69615232e
d8uv <d8uv@d8uv.org>
2010-11-16 10:55:34 -0900

    functionized circle

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 "use strict";
 
-onload = function () {
+onload = function () {  
+    Raphael.fn.growCircle = function (x, y) {
+	this.circle(x, y, 2)
+	    .attr({fill: "blue", stroke: "blue"})
+	    .hover(function (event) {
+		this.animate({scale: "20"}, 5000);
+	    }, function (event) {
+		this.animate({scale: "1"}, 5000, ">");
+	    });
+    };
+
     var paper = new Raphael(document.body, document.width, document.height);
-    
-    paper.circle(50, 50, 2)
-	.attr({fill: "blue", stroke: "blue"})
-	.hover(function (event) {
-	    this.animate({scale: "20"}, 5000);
-	}, function (event) {
-	    this.animate({scale: "1"}, 5000, ">");
-	});
+
+    paper.growCircle(100, 50);
 };
